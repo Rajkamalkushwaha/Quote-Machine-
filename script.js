8a225c8e7c951606b271d3af0a9dbc4eb1158b7e
Rajkamalkushwaha <36695253+Rajkamalkushwaha@users.noreply.github.com>
2018-03-30 14:10:52 +0530

    change code

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-
 $(document).ready(function () {
- 
- $('#new-quote').click(function () {
- var displayResources1 = $('#text');
- var displayResources2 =$('#author');
- displayResources1.text('Loading data from JSON source...');
- displayResources2.text('Loading data from JSON source...');
- 
- $.ajax({
- type: "GET",
- url: "result.json",
- success: function(result)
- {
- console.log(result);
+$('#new-quote').click(function () {
+	 
+	$.getJSON("result.json",function(result){
+		
+	 $.each(result,function(i,value){
+		$('#text').html(result[i].quote);
+		$('#author').html(result[i].author);
+		i+=1;
+	$(".quote-text").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('#text').text(result.quote);
+        });
 
- for (var i in result)
- {
- var output1=result[i].quota;
-  var output2=result[i].author ;
- }
-
- displayResources1.html(output1);
-  displayResource21.html(output2);
-
- }
- });
- 
- });
+      $(".quote-author").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('#author').html(result.author);
+		});
+		
+    });
 });
-
-
-
+	
+	var color=['#525530','#455500','#454533','#458566','#125425'];
+	 var rand=Math.floor(Math.random()*color.length);
+	  $("body").css("background-color",color[rand]);
+	         $(".button").css("color",color[rand]);
+			 $(".quote-box").css("color",color[rand]);
+        });
+ }); 
 
 
 
